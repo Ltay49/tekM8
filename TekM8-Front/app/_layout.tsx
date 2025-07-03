c18b6099439cbc3390cc6e2bff9e1e365f06b818
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-07-03 21:04:38 +0100

    connectivity

--- TekM8-Front/app/_layout.tsx
+++ TekM8-Front/app/_layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   if (!fontsLoaded) {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', borderBottomWidth: 0 }}>
         <Text>Loading fonts...</Text>
       </View>
     );
@@ -29,15 +29,34 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         title: 'Home',
         headerBackTitle: '',
         headerTitleAlign: isIndex ? 'left' : 'left',
+        headerTintColor: 'white',
+        headerShadowVisible: true, // Hide default shadow
+
         headerStyle: {
           backgroundColor: '#00AEEF',
         },
-        headerTintColor: 'black',
+
+        headerBackground: () => (
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: '#00AEEF',
+              shadowColor: '#fff',
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 1,
+              shadowRadius: 6,
+              elevation: 4, // Android
+            }}
+          />
+        ),
+
         headerTitleStyle: {
           fontFamily: 'Montserrat_700Bold',
-          fontSize: 18,
+          fontSize: 20,
         },
       }}
+
+
     >
       {children}
     </Stack>
